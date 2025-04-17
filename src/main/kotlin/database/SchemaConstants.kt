5f5348c7b364cee516376167f727209e6fa85e20
Brynjar-l <brynjar.sighvatsson@gmail.com>
2025-04-17 14:17:46 +0000

    (./database/) add SchemaConstants.kt

---
+++ src/main/kotlin/database/SchemaConstants.kt
@@ -0,0 +1,17 @@
+package database
+
+object TEXT {
+    object LIMIT {
+        const val DEFAULT = 255     // TEXT.LIMIT.DEFAULT
+        const val DOUBLE = 510      // TEXT.LIMIT.DOUBLE
+    }
+}
+
+object DECIMAL {
+    object PRECISION {
+        const val DEFAULT = 10      // DECIMAL.PRECISION.DEFAULT
+    }
+    object SCALE {
+        const val DEFAULT = 2       // DECIMAL.SCALE.DEFAULT
+    }
+}
